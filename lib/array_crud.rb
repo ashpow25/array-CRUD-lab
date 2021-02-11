e5fddf3bd77753db219e9d8d500f9ef74d35653a
Ashley <ashpow25@gmail.com>
2021-02-11 14:01:57 -0800

    commit tree 39165b12071e9f1cc371528a7ab9129c71520679

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
+a = []
+end
+
+def create_an_array 
+   a =  ["lily", "sunflower", "rose", "daisy"]  
   
 end
 
-def create_an_array
-  
+def add_element_to_end_of_array(array = a, element = "cactus")
+    array << element
+
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_start_of_array(array = a, element ="iris")
+    array.unshift(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(array )
+     cactus = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    iris = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array.last
 end
